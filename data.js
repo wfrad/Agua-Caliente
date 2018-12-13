690bf73b80d30edfdbc76fb6223086b305239d1b
wfrad <45856670+wfrad@users.noreply.github.com>
2018-12-13 10:33:19 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_pt2_stitch",
+      "name": "DJI_pt2_stitch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.529044044983463,
+          "pitch": -0.09384115490818523,
+          "title": "Tahquitz Waterfall",
+          "text": "Originally named Pal hani kalet by Ca wiske on ca leader of the Fox Tribe. This is a place of power."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
